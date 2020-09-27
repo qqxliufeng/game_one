9e42f8572d2be22da5080d4bb38b876ebbf4ebea
596484713@qq.com <liufeng>
2020-09-27 17:31:30 +0800

    测试用的

--- assets/script/Welcome.js
+++ assets/script/Welcome.js
@@ -1,3 +1,4 @@
+const { getReviewScene } = require("./utils/globals");
 
 cc.Class({
   extends: cc.Component,
@@ -7,6 +8,9 @@ cc.Class({
   },
 
   onLoad() {
-    getLoading().then((loadingController) => { this.loadingController = loadingController })
+    // getLoading().then((loadingController) => { this.loadingController = loadingController })
+    this.scheduleOnce(_ => {
+      cc.director.loadScene(getReviewScene())
+    }, 5)
   }
 });
