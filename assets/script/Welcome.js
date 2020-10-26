f973c74a9e7400569ac557167a4f577082ca21c8
596484713@qq.com <liufeng>
2020-10-26 17:56:42 +0800

    在入口页面添加分发游戏类型机制

--- assets/script/Welcome.js
+++ assets/script/Welcome.js
@@ -1,4 +1,4 @@
-const { getReviewScene } = require("./utils/globals");
+const { getReviewScene, getStudyScene, getGameScene, getApplyScene, getLevelScene } = require("./utils/globals");
 
 cc.Class({
   extends: cc.Component,
@@ -8,9 +8,30 @@ cc.Class({
   },
 
   onLoad() {
-    getLoading().then((loadingController) => { this.loadingController = loadingController })
-    // this.scheduleOnce(_ => {
-    //   cc.director.loadScene(getReviewScene())
-    // }, 5)
+    // 默认的游戏类型是---study
+    // 所有的游戏类型是：review(复习)、study(学习)、game(游戏)、apply(运用)、level(闯关)
+    const urlParams = new URLSearchParams(location.search)
+    const gameType = urlParams.get('type') || 'study'
+    let sceneName = ''
+    switch (gameType) {
+      case 'review':
+        sceneName = getReviewScene()
+        break
+      case 'study':
+        sceneName = getStudyScene()
+        break
+      case 'game':
+        sceneName = getGameScene()
+        break
+      case 'apply':
+        sceneName = getApplyScene()
+        break
+      case 'level':
+        sceneName = getLevelScene()
+        break
+      default:
+        sceneName = getStudyScene()
+    }
+    cc.director.loadScene(sceneName)
   }
 });
--- assets/script/utils/globals.js
+++ assets/script/utils/globals.js
@@ -1,16 +1,51 @@
 const WORD_ITEM_WIDTH = 100
 const REVIEW_GAME_SCENES = ['game_rabbit', 'game_bear', 'game_bird', 'game_dog', 'game_cat']
+const LEVEL_GAME_SCENES = ['game_level_bear', 'game_level_boat', 'game_level_car', 'game_level_draw', 'game_level_train', 'game_level_windmills']
 const WORD_PRE_FAB_NAME = 'prefab/word_item'
 
-console.log(WORD_PRE_FAB_NAME)
-
+/**
+ * 获得一个复习的游戏场景
+ */
 function getReviewScene() {
   return REVIEW_GAME_SCENES[Math.floor(Math.random() * REVIEW_GAME_SCENES.length)]
 }
+
+/**
+ * 获得一个闯关的游戏场景
+ */
+function getLevelScene() {
+  return LEVEL_GAME_SCENES[Math.floor(Math.random() * LEVEL_GAME_SCENES.length)]
+}
+
+/**
+ * 获得一个学习的游戏场景
+ */
+function getStudyScene() {
+  return 'game_study'
+}
+
+/**
+ * 获得一个运用的游戏场景
+ */
+function getApplyScene() {
+  return 'game_apply'
+}
+
+/**
+ * 获得一个游戏的游戏场景
+ */
+function getGameScene() {
+  return 'game_game'
+}
+
 module.exports = {
   WORD_PRE_FAB_NAME,
   WORD_ITEM_WIDTH,
   getReviewScene,
+  getLevelScene,
+  getStudyScene,
+  getApplyScene,
+  getGameScene,
   getSpriteSize(sprite, width = WORD_ITEM_WIDTH) {
     return { width, height: parseInt(width * sprite.getRect().height / sprite.getRect().width) }
   }
